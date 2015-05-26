4645320dce908e47bfc3a5c006a44da76b5a55df
EPS <eps@clL03-6.eps.ua.es>
2015-05-26 19:40:02 +0200

    Initial Commit

---
+++ editorRTF/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  editorRTF
+//
+//  Created by EPS on 26/05/15.
+//  Copyright (c) 2015 EPS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ editorRTF/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  editorRTF
+//
+//  Created by EPS on 26/05/15.
+//  Copyright (c) 2015 EPS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
